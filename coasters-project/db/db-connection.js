345419ebf18b7689375e79766619d1e0dfa915d6
Gimena Motto <gimenapimba@gmail.com>
2023-02-08 22:13:09 +0100

    done

---
+++ coasters-project/db/db-connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const databaseName = 'themepark'
+const connectionString = `mongodb://127.0.0.1/${databaseName}`
+
+mongoose
+    .set('strictQuery', false)
+    .connect(connectionString)
+    .then(connectionInfo => console.log(`Connected to Mongo! Database name: "${connectionInfo.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err))
