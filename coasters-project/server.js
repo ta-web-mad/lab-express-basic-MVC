e6a9d4cc881e5eecd717367b5996b9bc8d113cda
aldidjamil <alditamadjamil@gmail.com>
2023-02-08 23:24:45 +0100

    done

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,41 @@
 // Happy coding! Party! 
+
+const express = require('express')
+
+const hbs = require('hbs')
+
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+require('./db/db-connection')
+
+const Coaster = require('./models/coaster.model')
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/coaster-gallery', (req, res) => {
+    Coaster
+        .find()
+        .then(allCoasters => {
+            res.render('coaster-gallery', { allCoasters })
+            console.log(allCoasters)
+
+        })
+
+
+})
+
+
+
+app.listen(5005, () => console.log('🏃‍ on port 5005'));
+
+
