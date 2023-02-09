f0bb9d59b0c9e2e33a8f94c6aff0195befb99f49
Diego Rodriguez <godiedri@gmail.com>
2023-02-09 09:03:57 +0100

    done

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,48 @@
 // Happy coding! Party! 
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// DDBB
+require('./db/db-connection')
+
+// Model
+const Coaster = require('./models/coaster.model')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/coasters-gallery', (req, res) => {
+    Coaster
+        .find()
+        .then(coasters => {
+            res.render('coasters-gallery', { coasters: coasters })
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/coaster-longest', (req, res) => {
+    Coaster
+        .find({length: {$gt: 1000} })
+        .then(coasters => {
+            res.render('coaster-longest', { coasters: coasters })
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/coaster-craziest', (req, res) => {
+    Coaster
+        .find({inversions: {$gt: 3} })
+        .then(coasters => {
+            res.render('coaster-craziest', { coasters: coasters })
+        })
+        .catch(err => console.log(err))
+})
+
+app.listen(5005, () => console.log('APP LISTENING'))
