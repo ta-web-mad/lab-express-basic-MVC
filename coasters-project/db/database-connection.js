035f62f19a3608830ed041993469bc6e68a8aa5b
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-03 19:12:49 +0200

    Realizado al 100%

---
+++ coasters-project/db/database-connection.js
@@ -0,0 +1,13 @@
+
+
+
+const mongoose = require('mongoose')
+
+const databaseName = 'themepark'
+const connectionString = `mongodb://localhost/${databaseName}`
+
+mongoose
+    .connect(connectionString)
+    .then(connectionInfo => console.log(`Hey! -> Connected to Mongo! Database name: "${connectionInfo.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err))
+
