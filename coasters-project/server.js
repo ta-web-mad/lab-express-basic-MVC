2ea60f2949e218939835a418b35a00c98c8c1737
Pedro de la Torre <pedrodltl@gmail.com>
2023-05-03 20:12:50 +0200

    Lab terminadogit status

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,41 @@
-// Happy coding! Party! 
+require('./db/database-connection')
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
+const Coaster = require('./models/coaster.model')
+
+
+app.get('/', (req, res) => {
+    res.render('index-page')
+})
+
+app.get('/coasters-page', (req, res) => {
+
+    Coaster
+        .find().sort({ title: 1 })
+        .then(allCoasters => res.render('coasters-page', { coasters: allCoasters }))
+        .catch(err => console.log(err))
+})
+
+app.get('/longest-page', (req, res) => {
+
+    Coaster
+        .find({ length: { $gt: 100 } })
+        .then(mostLongest => res.render('longest-page', { coasterLong: mostLongest }))
+        .catch(err => console.log(err))
+})
+
+app.get('/craziest-page', (req, res) => {
+    Coaster
+        .find({ inversions: { $gt: 3 } })
+        .then(mostCraziest => res.render('craziest-page', { coasterCrazy: mostCraziest }))
+        .catch(err => console.log(err))
+})
+
+app.listen(5005, () => console.log('SERVIDOR LEVANTADO EN 5005'))
+
