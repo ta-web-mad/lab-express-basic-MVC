d9f43f534708f248fe3913434a37e6af51d2a01f
sergiolopez <sergio.lopez.souto@gmail.com>
2023-05-03 22:17:33 +0200

    done

---
+++ server.js
@@ -0,0 +1,48 @@
+
+require('./db/db-connection')
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
+const data = require('./coasters-data');
+
+const Coaster = require('./models/coaster.model')
+
+// Coaster.create(data)
+//     .then(console.log("base de datos creada con data.json"))
+//     .catch(err => console.log("ERROR:" , err))
+//
+// Coaster.deleteMany()
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/coasters', (req, res) => {
+    Coaster
+    .find().sort( {name:1} )
+    .then(arrayCoasters => res.render('coasters' , {coaster: arrayCoasters}))
+    .catch(err => console.log("ERROR:" , err))
+
+})
+
+app.get('/longest', (req, res) => {
+    Coaster.find({length: {$gte: 1000}})
+    .then(arrayCoasters => res.render('longest' , {coaster: arrayCoasters}))
+    .catch(err => console.log("ERROR:" , err))
+
+})
+
+app.get('/craziest', (req, res) => {
+    Coaster.find({inversions: {$gt: 3}})
+    .then(arrayCoasters => res.render('craziest' , {coaster: arrayCoasters}))
+    .catch(err => console.log("ERROR:" , err))
+})
+
+
+app.listen(3000, () => console.log('SERVIDOR LEVANTADO EN 3000'))
