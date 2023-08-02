4816d13df561976de09e912a05b73cc1b41a852a
samuelemarino04 <samuele12601@gmail.com>
2023-08-02 19:20:40 +0200

    done

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,29 @@
-// Happy coding! Party! 
+require('./db/database-connection')
+
+const Coaster = require('./models/coaster.model')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
+
+app.get('/', (req, res) => {
+    res.render('home-page')
+})
+
+app.get('/coasters-gallery', (req, res) => {
+
+    Coaster
+        .find()
+        .sort({ title: 1 })
+        .then(coasters => res.render('coasters-gallery', { coasters }))
+        .catch(err => console.log(err))
+})
+
+
+
+
+app.listen(5005, () => console.log('IS WORKING!'))
