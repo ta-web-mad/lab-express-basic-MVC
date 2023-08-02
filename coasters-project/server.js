3b9e82a2f6feac4cadef4be1bea1ae35f607bead
Lortes <lortes95@gmail.com>
2023-08-02 16:31:53 +0200

    navbar

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -10,37 +10,34 @@ app.set('view engine', 'hbs')
 const Coaster = require('./models/coaster.model')
 
 
-app.get("/", ( req,res ) => {
+app.get("/", (req, res) => {
     res.render("index-page")
 })
 
-app.get("/coasters-gallery", (req, res)=>{
-
+app.get("/coasters-gallery", (req, res) => {
 
     Coaster
-            .find()
-            .sort({ title: 1 }) 
-            .then(allCoasters =>  res.render("coasters", {coasters: allCoasters}))
-            .catch(err => console.log(err))
+        .find()
+        .sort({ title: 1 })
+        .then(allCoasters => res.render("coasters", { coasters: allCoasters }))
+        .catch(err => console.log(err))
 
 })
 
-app.get("/longest", (req, res)=>{
-   // res.send("LONGEST")
+app.get("/longest", (req, res) => {
 
     Coaster
-            .find({ length: {$gt: 1000} })
-            .then(allCoastersLongest =>  res.render("coasters-longest", {coastersLongest: allCoastersLongest}))
-            .catch(err => console.log(err))
+        .find({ length: { $gt: 1000 } })
+        .then(allCoastersLongest => res.render("coasters-longest", { coastersLongest: allCoastersLongest }))
+        .catch(err => console.log(err))
 })
 
-app.get("/craziest", (req, res)=>{
-   // res.send("LONGEST")
+app.get("/craziest", (req, res) => {
 
     Coaster
-            .find({ inversions: {$gt: 3} })
-            .then(allCoastersCraziest =>  res.render("coasters-craziest", {coastersCraziest: allCoastersCraziest}))
-            .catch(err => console.log(err))
+        .find({ inversions: { $gt: 3 } })
+        .then(allCoastersCraziest => res.render("coasters-craziest", { coastersCraziest: allCoastersCraziest }))
+        .catch(err => console.log(err))
 })
 
 
