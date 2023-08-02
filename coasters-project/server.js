a5b2a7d8650adbab2b887a7ad8515ed707fe2df0
AlexOcana <a.ocana.monsalve@hotmail.com>
2023-08-02 19:47:33 +0200

    DONE

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,28 @@
-// Happy coding! Party! 
+const express = require('express')
+
+const app = express()
+
+
+app.use(express.static('public'))
+app.set("views", `${__dirname}/views`)
+app.set("view engine", "hbs")
+
+require('./bd/databse-connection')
+
+const Coaster = require('./models/coaster.model')
+
+
+app.get("/", (req, res) => {
+
+    res.render("index-page")
+})
+
+app.get("/coasters-gallery", (req, res) => {
+
+    Coaster
+        .find()
+        .then(coasters => res.render('coasters-gallery', { coasters }))
+        .catch(err => console.log(err))
+})
+
+app.listen(5005, () => console.log('RUNNING ON PORT 5005'))
