8f9efa951d9bf0ec72d460750fa1553dfca2d775
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-01 20:56:47 +0100

    finish

---
+++ coasters-project/models/coaster.model.js
@@ -0,0 +1,15 @@
+
+
+const { Schema, model } = require('mongoose')
+
+const coasterSchema = new Schema({
+    title: String,
+    description: String,
+    length: Number,
+    inversions: Number,
+    imageUrl: String
+})
+
+const Coaster = model('coaster', coasterSchema)
+
+module.exports = Coaster
