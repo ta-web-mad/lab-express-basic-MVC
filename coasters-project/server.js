547894fd868d9d3029a6c31e59603c56bf857b4d
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-11-01 19:27:12 +0100

    solved lab

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,31 @@
 // Happy coding! Party! 
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
+require('./db/database-connection')
+const Coaster = require('./models/coaster.model')
+
+app.get('/', (req, res) => {
+   // res.send('holi?')
+   res.render('home-page')
+})
+
+app.get('/galeria',(req, res) =>{
+   // res.send('holaaaa')
+  // res.render('gallery')
+
+   Coaster
+   .find()
+   .sort({title: 1})
+   .then(coasters=> res.render('gallery', {coast: coasters}))
+   .catch(err => console.log('ERROR:', err))
+})
+
+
+
+app.listen(5005, () => console.log('Server listening on port 5005'))
