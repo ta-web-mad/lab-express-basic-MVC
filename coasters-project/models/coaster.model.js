975300f58a3bb93fb78f8e6bc8a4b01740f05165
Fabio García <fabiogcaam@gmail.com>
2023-11-01 20:27:13 +0100

    lab solved

---
+++ coasters-project/models/coaster.model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose')
+
+const coasterSchema = new Schema({
+    title: String,
+    description: String,
+    length: Number,
+    inversions: Number,
+    imageUrl: String
+})
+
+
+const Coaster = model('coaster', coasterSchema)
+
+module.exports = Coaster
